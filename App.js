79e3ab3d4505ff81c3de4f8e116d90826bd0e45e
Vitor.Dev <contato@vitor.dev.br>
2021-12-26 22:37:49 -0300

    Meu primeiro componente!

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import React from "react"
+import { Text } from 'react-native'
 
-export default function App() {
-  return (
-    // SafeAreaView faz com quê o conteúdo da View seja correspondente a área útil do IOS.
-    // No Android a tela começa atrás do relógio e indicadores de aplicativos
-    // No Iphone ele fica atrás da faixa preta no topo da tela
-    <SafeAreaView style={styles.container}>
-      <Text>Olá!</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+function App () {
+  // Eu posso salvar o conteúdo em uma var
+  // const jsx = <Text>Mensagem exemplo!</Text>
+  // return jsx
+  return <Text>Mensagem exemplo!</Text>
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    flexDirection: 'row'
-  },
-});
+export default App
