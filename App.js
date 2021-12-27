680c4f0a90948a7db576137a9c516d89ef60d965
Vitor.Dev <contato@vitor.dev.br>
2021-12-26 22:46:30 -0300

    Componentes com Funções Arrow e Anônimas

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import React from "react"
 import { Text } from 'react-native'
 
-function App () {
-  // Eu posso salvar o conteúdo em uma var
-  // const jsx = <Text>Mensagem exemplo!</Text>
-  // return jsx
+/*
+// OPÇÃO 1
+// Deixando o componente como função anônima
+const App = function () {
   return <Text>Mensagem exemplo!</Text>
 }
 
 export default App
+*/
+
+// OPÇÃO 2
+// Deixando o componente como arrow funcion
+export default () => {
+  return <Text>Mensagem exemplo!</Text>
+}
