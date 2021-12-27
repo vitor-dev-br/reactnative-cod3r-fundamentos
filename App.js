b8dc8fd11ca4b1f4ef6d097369b8df0ebcca3af4
Vitor.Dev <contato@vitor.dev.br>
2021-12-26 22:30:37 -0300

    View vs SafeAreaView

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { StyleSheet, Text, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
+    // SafeAreaView faz com quê o conteúdo da View seja correspondente a área útil do IOS.
+    // No Android a tela começa atrás do relógio e indicadores de aplicativos
+    // No Iphone ele fica atrás da faixa preta no topo da tela
     <SafeAreaView style={styles.container}>
       <Text>Olá!</Text>
       <StatusBar style="auto" />
