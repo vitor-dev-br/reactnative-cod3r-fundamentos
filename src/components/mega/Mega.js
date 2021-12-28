0d039c50698f04521add2835d58e3d6778e8792a
Vitor.Dev <contato@vitor.dev.br>
2021-12-28 02:57:39 -0300

    Estado em componente com class

--- src/components/mega/Mega.js
+++ src/components/mega/Mega.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { Text } from 'react-native'
 
 export default class Mega extends Component {
 
+    // Forma 1 de iniciar o estado
+    state = {
+        qtdNumeros: this.props.qtdNumeros
+    }
+    /*
+    // Forma 2 de iniciar o estado
+    constructor(props) {
+        super(props)
+        this.state = {
+            qtdNumeros: props.qtdNumeros
+        }
+    }
+    */
+
     render() {
         return (
-            <Text>Componente by Classe {this.props.nome}.</Text>
+            <Text>Componente by Classe {this.state.qtdNumeros}.</Text>
         )
     }
 }
